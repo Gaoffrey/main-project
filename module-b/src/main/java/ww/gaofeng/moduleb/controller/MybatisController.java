76da452c4a16dd391c4ba2c70a7a64841f9314ff
Gaoffrey <1425417918@qq.com>
2020-08-02 17:57:55 +0800

    module b add mybatis selectAll

--- module-b/src/main/java/ww/gaofeng/moduleb/controller/MybatisController.java
+++ module-b/src/main/java/ww/gaofeng/moduleb/controller/MybatisController.java
@@ -1,10 +1,13 @@
 package ww.gaofeng.moduleb.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import ww.gaofeng.moduleb.bean.User;
 import ww.gaofeng.moduleb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MybatisController {
@@ -19,10 +22,27 @@ public class MybatisController {
         user.setName("ww1");
         user.setTel("182");
 
-        Integer result =userService.insert(user);
-        System.out.println(result);
+        userService.insert(user);
 
         return user;
+    }
+
+    @RequestMapping("m_select/{id}")
+    public User m_select(@PathVariable("id") Integer id) {
+
+        User user =userService.selectByPrimaryKey(id);
+
+        System.out.println(user);
+        return user;
+    }
+
+    @RequestMapping("m_all")
+    public List<User> m_all() {
+
+        List<User> userList =userService.selectList();
+
+        System.out.println(userList);
+        return userList;
     }
 
     @RequestMapping("m_hello")
