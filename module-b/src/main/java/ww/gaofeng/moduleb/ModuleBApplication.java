9d4fd705911ccd3ebdb2b6742fb4a3374b02a176
Gaoffrey <1425417918@qq.com>
2020-08-02 17:19:38 +0800

    module b add mybatis @MapperScan

--- module-b/src/main/java/ww/gaofeng/moduleb/ModuleBApplication.java
+++ module-b/src/main/java/ww/gaofeng/moduleb/ModuleBApplication.java
@@ -1,11 +1,13 @@
 package ww.gaofeng.moduleb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("ww.gaofeng.moduleb.mapper")     //扫描mapper接口
 public class ModuleBApplication {
 
     public static void main(String[] args) {
